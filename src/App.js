f759a903bdff23e5fd61b46052fc0bb00aa1847a
Shatakshi <shatakshi.1052@gmail.com>
2025-01-28 11:25:10 -0500

    Final Front end design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from "react";
+
+import logo from './logo.svg'
+
 import './assests/css/App.css'
+import './assests/css/Sentiment.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<div className="dashboard-container">
+				<div className="dashboard-card">
+					<h1 className="dashboard-title">Sentiment Analysis</h1>
+					
+					<form className="dashboard-form">
+						<textarea className="dashboard-textarea" rows="6" placeholder="Enter text to analyze" value='value'></textarea>
+						<button type="submit" className="dashboard-button"> Analyze Sentiment </button>
+					</form>
+					
+					<footer className="dashboard-footer">
+						Designed by <strong>Shatakshi Singh</strong> for FIYGE Research.
+					</footer>
+				</div>
+			</div>
+		</React.Fragment>
+	)
 }
 
-export default App;
+export default App
