d4f21096c571249cc266e3899291094c3b2e5210
Shatakshi <shatakshi.1052@gmail.com>
2025-01-28 11:36:39 -0500

    Updates on frontend form fields

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from "react";
-
-import logo from './logo.svg'
+import React, { useState } from "react"
 
 import './assests/css/App.css'
 import './assests/css/Sentiment.css'
 
 function App() {
+	const [text, setText] = useState("")
+	
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+	}
+
 	return (
 		<React.Fragment>
 			<div className="dashboard-container">
 				<div className="dashboard-card">
 					<h1 className="dashboard-title">Sentiment Analysis</h1>
 					
-					<form className="dashboard-form">
-						<textarea className="dashboard-textarea" rows="6" placeholder="Enter text to analyze" value='value'></textarea>
-						<button type="submit" className="dashboard-button"> Analyze Sentiment </button>
+					<form onSubmit={handleSubmit} className="dashboard-form">
+						<textarea className="dashboard-textarea" rows="6" placeholder="Enter text to analyze" 
+									value={text}
+									onChange={(e) => setText(e.target.value)}
+						></textarea>
+						<button type="submit" className="dashboard-button" disabled> Analyze Sentiment </button>
 					</form>
 					
 					<footer className="dashboard-footer">
